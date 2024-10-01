25d97669a566a07fd912cdc23356330c95c133a4
Pedro Rodrigues <evocloudpedro@gmail.com>
2024-10-01 01:17:43 +0000

    Solucao do desafio

--- challenge/challeng0003.rb
+++ challenge/challeng0003.rb
@@ -15,4 +15,48 @@ def stock_picker(days)
 
 end
 
-p stock_picker([1, 2, 5, 10, 2, 6])
+
+def stock_picker_teste(stock)
+    index = 0
+
+    buy_day = 0
+    sell_day = 0
+
+    result = []
+
+    #compra
+    for i in stock do
+        if stock[(index + 1)].nil?
+            value = stock[index] < stock[0]
+        else
+            value = stock[index] < stock[(index + 1)]
+        end
+        index +=1
+        unless value
+            result.push(index)
+            break
+        end
+
+    end
+
+    #venda 
+    for i in stock do
+        if stock[(index + 1)].nil?
+            value = stock[index] > stock[0]
+        else
+            value = stock[index] > stock[(index + 1)]
+        end
+        index +=1
+        
+        if value
+            index -=1
+            result.push(index)
+            break
+        end
+
+    end
+
+    return result
+end
+
+p stock_picker_teste([17,3,6,9,15,8,6,1,10])
